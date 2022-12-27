e7c485f4861af50369b3fac6786b4c4be7ac59ac
Mridul Roy <39593927+d4rkr0n1n@users.noreply.github.com>
2022-12-27 07:17:55 +0530

    Add files via upload

---
+++ src/main/java/com/survey/app/config/WebSecurityConfig.java
@@ -0,0 +1,43 @@
+package com.survey.app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(
+				requests -> requests.requestMatchers("/", "/home").permitAll().anyRequest().authenticated())
+				.formLogin(form -> form.loginPage("/login").permitAll()).logout(LogoutConfigurer::permitAll);
+
+		return http.build();
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+		manager.createUser(
+				User.withUsername("user").password(passwordEncoder().encode("redhat")).roles("USER").build());
+		manager.createUser(
+				User.withUsername("admin").password(passwordEncoder().encode("redhat")).roles("ADMIN").build());
+		return manager;
+
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
---
+++ src/main/java/com/survey/app/controller/SurveyAppController.java
@@ -0,0 +1,30 @@
+package com.survey.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SurveyAppController {
+	 @RequestMapping(value = "/")
+	public String viewHomePage() {
+
+		return "home";
+	}
+	 @RequestMapping(value = "/login")
+	 public String viewLoginPage() {
+		 viewHelloPage();
+		 return "login";
+	 }
+	 @RequestMapping(value = "/home")
+	 public String viewPage() {
+		 
+		 return "home";
+	 }
+	 @RequestMapping(value = "/hello")
+	 public String viewHelloPage() {
+		 
+		 return "hello";
+	 }
+
+}
